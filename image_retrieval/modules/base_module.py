aad2df1c66a1bb4fcb02fa18983f87275d6c00b4
samsja <sami.jaghouar@hotmail.fr>
2023-01-29 13:13:29 +0100

    feat: add knn accuracy
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- image_retrieval/modules/base_module.py
+++ image_retrieval/modules/base_module.py
@@ -1,6 +1,9 @@
+from typing import NamedTuple, Optional
+
 import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader
+from torchmetrics.functional import accuracy as accuracy_fn
 from torchmetrics.functional import retrieval_average_precision
 
 from image_retrieval.metrics import cosine_sim
@@ -20,9 +23,17 @@ class BaseRetrievalMixin(pl.LightningModule):
         )
 
     def on_validation_epoch_end(self) -> None:
-        self.log(
-            "val_map", self.retrieval_metrics.on_validation_epoch_end(), prog_bar=True
-        )
+        metrics = self.retrieval_metrics.on_validation_epoch_end()
+        if metrics.accuracy:
+            self.log("val_accuracy", metrics.accuracy, prog_bar=True)
+
+        if metrics.map:
+            self.log("val_map", metrics.map, prog_bar=True)
+
+
+class RetrievalMetrics(NamedTuple):
+    map: Optional[torch.Tensor] = None
+    accuracy: Optional[torch.Tensor] = None
 
 
 class RetrievalHelper:
@@ -49,16 +60,20 @@ class RetrievalHelper:
         self.val_sim.append(cosine_sim(self.query_embeddings, features))
         self.val_labels.append(labels)
 
-    def on_validation_epoch_end(self) -> float:
+    def on_validation_epoch_end(self) -> RetrievalMetrics:
         self.val_sim = torch.concat(self.val_sim, dim=1)
         self.val_labels = torch.concat(self.val_labels)
 
         preds = torch.stack([label == self.val_labels for label in self.query_labels])
         map = retrieval_average_precision(self.val_sim, preds)
 
+        _, top_index = self.val_sim.topk(1, dim=1, largest=True, sorted=True)
+        top_class = self.val_labels[top_index]
+        accuracy = accuracy_fn(top_class, self.query_labels.to(top_class.device))
+
         del self.query_embeddings
         del self.query_labels
         self.query_embeddings = None
         self.query_labels = None
 
-        return map
+        return RetrievalMetrics(map, accuracy)
