fix(simproto): use batchmean as reduction in kl div loss
Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

image_retrieval/modules/ssl/simproto.py
@@ -17,7 +17,7 @@ class SimProto(BaseRetrievalMixin):
         model: torch.nn.Module,
         data: pl.LightningDataModule,
         lr=1e-3,
-        dim=1000,
+        dim=100,
         temp_target=0.025,
         temp_anchor=0.1,
         lambda_=1,
@@ -35,7 +35,7 @@ class SimProto(BaseRetrievalMixin):
 
         prev_dim = model.output_size
         self.fc = nn.Linear(prev_dim, dim)
-        self.loss_fn = torch.nn.KLDivLoss(log_target=True)
+        self.loss_fn = torch.nn.KLDivLoss(log_target=True, reduction="batchmean")
 
     def forward(self, x: TensorType["batch_size":...]) -> TensorType["batch_size":...]:
         x = self.model(x)
@@ -44,26 +44,30 @@ class SimProto(BaseRetrievalMixin):
     def training_step(self, batch, batch_idx):
         x, _ = batch
         target, anchor = x[0], x[1]
+
         self.log("fc_std", self.fc.weight.std(), prog_bar=True)
-        logits_target: object = F.log_softmax(
+
+        logits_target = F.log_softmax(
             self.fc(self.model(target)) / self.temp_target, dim=1
         ).detach()
+
         logits_anchor = F.log_softmax(
             self.fc(self.model(anchor)) / self.temp_anchor, dim=1
         )
+
         class_std = logits_anchor.argmax(dim=1).float().std()
         self.log("class_std", class_std)
+
         me_max = (
             self.lambda_ * logits_anchor.mean()
         )  # regularize to avoid prototypes to collapse
+        self.log("me_max", me_max)
 
         kl_div = self.loss_fn(logits_anchor, logits_target)
+        self.log("kl_div", kl_div)
 
         loss = kl_div - me_max
-
         self.log("train_loss", loss)
-        self.log("kl_div", kl_div)
-        self.log("me_max", me_max)
 
         return loss
 